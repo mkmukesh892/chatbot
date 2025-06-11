fb7b5f74b8496b7ed0ab483cb5f21635ac91f670
mukeshkumar <mukeshkumar@fico.com>
2025-06-11 10:24:28 +0530

    adjusted api parameter based on backend code and added pre

--- src/app/chatbot/chatbot.service.ts
+++ src/app/chatbot/chatbot.service.ts
@@ -6,6 +6,6 @@ import { inject, Injectable } from "@angular/core"
 export class ChatBotService{
     private http = inject(HttpClient);
     getAIResponse(userMsg: string){
-        return this.http.post<any>('http://localhost:8080/api/chat', { message: userMsg });
+        return this.http.post<any>('http://localhost:8080/api/chat', { question: userMsg });
     }
 }
